84c046509373cfceea5695045ab0319c92dfbee2
Alex <alex@plshark.net>
2017-12-09 09:23:07 -0500

    Add embedded h2 database

--- notes-server/src/main/java/net/plshark/notes/webservice/Application.java
+++ notes-server/src/main/java/net/plshark/notes/webservice/Application.java
@@ -1,7 +1,5 @@
 package net.plshark.notes.webservice;
 
-import javax.sql.DataSource;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -26,10 +24,5 @@ public class Application {
      */
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    public DataSource dataSource() {
-        // TODO
-        return null;
     }
 }
