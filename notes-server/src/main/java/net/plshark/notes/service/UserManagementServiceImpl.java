449226451515e89b61b1add5bdf30378667c6cc7
Alex <alex@plshark.net>
2017-12-17 20:48:14 -0500

    Encode password before saving

--- notes-server/src/main/java/net/plshark/notes/service/UserManagementServiceImpl.java
+++ notes-server/src/main/java/net/plshark/notes/service/UserManagementServiceImpl.java
@@ -1,5 +1,9 @@
 package net.plshark.notes.service;
 
+import java.util.Objects;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 import net.plshark.notes.Role;
 import net.plshark.notes.User;
 import net.plshark.notes.repo.RoleRepository;
@@ -12,21 +16,29 @@ public class UserManagementServiceImpl implements UserManagementService {
 
     private final UserRepository userRepo;
     private final RoleRepository roleRepo;
+    private final PasswordEncoder passwordEncoder;
 
     /**
      * Create a new instance
      * @param userRepository the repository for accessing users
      * @param roleRepository the repository for accessing roles
+     * @param passwordEncoder the encoder to use to encode passwords
      */
-    public UserManagementServiceImpl(UserRepository userRepository, RoleRepository roleRepository) {
+    public UserManagementServiceImpl(UserRepository userRepository, RoleRepository roleRepository,
+            PasswordEncoder passwordEncoder) {
         this.userRepo = userRepository;
         this.roleRepo = roleRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
     public User saveUser(User user) {
         if (user.getId().isPresent())
             throw new IllegalArgumentException("Updating a user is not supported");
+        Objects.requireNonNull(user.getUsername(), "username cannot be null");
+        Objects.requireNonNull(user.getPassword(), "password cannot be null");
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
 
         return userRepo.insert(user);
     }
