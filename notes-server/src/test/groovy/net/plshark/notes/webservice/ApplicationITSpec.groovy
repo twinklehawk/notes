c09437c8a00b4e5351d8b476e6577291eb55043c
Alex <alex@plshark.net>
2017-12-31 10:52:07 -0500

    Convert application test to spock

---
+++ notes-server/src/test/groovy/net/plshark/notes/webservice/ApplicationITSpec.groovy
@@ -0,0 +1,34 @@
+package net.plshark.notes.webservice
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
+
+import javax.inject.Inject
+
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.web.WebAppConfiguration
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.setup.MockMvcBuilders
+import org.springframework.web.context.WebApplicationContext
+
+import com.fasterxml.jackson.databind.ObjectMapper
+
+import net.plshark.notes.Note
+import spock.lang.Specification
+
+@SpringBootTest(classes = Application.class)
+@ActiveProfiles("test")
+class ApplicationITSpec extends Specification {
+
+    @Inject
+    Application application
+
+    def "context can be built"() {
+        expect:
+        application != null
+    }
+}
