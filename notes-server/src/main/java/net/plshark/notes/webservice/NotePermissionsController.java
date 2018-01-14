40ec23f571512abb610b453ce1acc219d6a3666f
Alex <alex@plshark.net>
2018-01-14 00:23:52 -0500

    Add controller for modifying user permissions on notes

---
+++ notes-server/src/main/java/net/plshark/notes/webservice/NotePermissionsController.java
@@ -0,0 +1,69 @@
+package net.plshark.notes.webservice;
+
+import java.util.Objects;
+
+import org.springframework.http.MediaType;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import net.plshark.notes.NotePermission;
+import net.plshark.notes.ObjectNotFoundException;
+import net.plshark.notes.service.NotePermissionsService;
+import net.plshark.notes.service.UserAuthenticationService;
+
+/**
+ * Controller for modifying a user's permissions for a note
+ */
+@RestController
+@RequestMapping("/notes/{id}/user-permissions")
+public class NotePermissionsController {
+
+    private final NotePermissionsService notePermissionsService;
+    private final UserAuthenticationService userAuthService;
+
+    /**
+     * Create a new instance
+     * @param notePermissionsService the service to use for note permissions
+     * @param userAuthService the service to use to retrieve authenticated user information
+     */
+    public NotePermissionsController(NotePermissionsService notePermissionsService,
+            UserAuthenticationService userAuthService) {
+        this.notePermissionsService = Objects.requireNonNull(notePermissionsService,
+                "notePermissionsService cannot be null");
+        this.userAuthService = Objects.requireNonNull(userAuthService, "userAuthService cannot be null");
+    }
+
+    /**
+     * Set a user's permissions for a note
+     * @param id the note ID
+     * @param userId the user ID of the user to set permissions for
+     * @param permission the permissions to set
+     * @param auth the currently authenticated user
+     * @throws ObjectNotFoundException if the note is not found for the user
+     */
+    @PostMapping(path = "/{userId}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void setPermissionForUser(@PathVariable("id") long id, @PathVariable("userId") long userId,
+            @RequestBody NotePermission permission, Authentication auth) throws ObjectNotFoundException {
+        long currentUserId = userAuthService.getUserIdForAuthentication(auth);
+        notePermissionsService.setPermissionForUser(id, userId, currentUserId, permission);
+    }
+
+    /**
+     * Remove all permissions for a note from a user
+     * @param id the note ID
+     * @param userId the user ID of the user to remove permissions from
+     * @param auth the currently authenticated user
+     * @throws ObjectNotFoundException if the note is not found for the user
+     */
+    @DeleteMapping(path = "/{userId}")
+    public void removePermissionForUser(@PathVariable("id") long id, @PathVariable("userId") long userId,
+            Authentication auth) throws ObjectNotFoundException {
+        long currentUserId = userAuthService.getUserIdForAuthentication(auth);
+        notePermissionsService.removePermissionForUser(id, userId, currentUserId);
+    }
+}
---
+++ notes-server/src/test/groovy/net/plshark/notes/webservice/NotePermissionsControllerSpec.groovy
@@ -0,0 +1,49 @@
+package net.plshark.notes.webservice
+
+import org.springframework.security.core.Authentication
+
+import net.plshark.notes.NotePermission
+import net.plshark.notes.service.NotePermissionsService
+import net.plshark.notes.service.UserAuthenticationService
+import spock.lang.Specification
+
+class NotePermissionsControllerSpec extends Specification {
+
+    NotePermissionsService notePermissionService = Mock()
+    UserAuthenticationService userAuthService = Mock()
+    NotePermissionsController controller = new NotePermissionsController(notePermissionService, userAuthService)
+
+    def "nulls not allowed in constructor args"() {
+        when:
+        new NotePermissionsController(null, userAuthService)
+
+        then:
+        thrown(NullPointerException)
+
+        when:
+        new NotePermissionsController(notePermissionService, null)
+
+        then:
+        thrown(NullPointerException)
+    }
+
+    def "setting permissions looks up the user ID"() {
+        userAuthService.getUserIdForAuthentication(_) >> 12
+
+        when:
+        controller.setPermissionForUser(1, 2, NotePermission.create(true, true), Mock(Authentication))
+
+        then:
+        1 * notePermissionService.setPermissionForUser(1, 2, 12, NotePermission.create(true, true))
+    }
+
+    def "removing permissions looks up the user ID"() {
+        userAuthService.getUserIdForAuthentication(_) >> 12
+
+        when:
+        controller.removePermissionForUser(1, 2, Mock(Authentication))
+
+        then:
+        1 * notePermissionService.removePermissionForUser(1, 2, 12)
+    }
+}
