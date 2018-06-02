14479b7c8a2baf3e1dc9ebf879aaba06207f48a7
Alex <alex@plshark.net>
2018-06-02 16:22:32 -0400

    Fix net.plshark.notes.webservice tests

--- notes-server/src/test/groovy/net/plshark/notes/webservice/UsersControllerSpec.groovy
+++ notes-server/src/test/groovy/net/plshark/notes/webservice/UsersControllerSpec.groovy
@@ -4,6 +4,9 @@ import net.plshark.BadRequestException
 import net.plshark.users.PasswordChangeRequest
 import net.plshark.users.User
 import net.plshark.users.service.UserManagementService
+import reactor.core.publisher.Mono
+import reactor.test.StepVerifier
+import reactor.test.publisher.PublisherProbe
 import spock.lang.Specification
 
 class UsersControllerSpec extends Specification {
@@ -28,44 +31,59 @@ class UsersControllerSpec extends Specification {
     }
 
     def "password is not returned when creating a user"() {
-        service.saveUser(_) >> new User(1, "user", "pass-encoded")
+        service.saveUser(_) >> Mono.just(new User(1, "user", "pass-encoded"))
 
-        when:
-        User user = controller.insert(new User("name", "pass"))
-
-        then:
-        user.password.present == false
+        expect:
+        StepVerifier.create(controller.insert(new User("name", "pass")))
+            .expectNextMatches({User user -> user.password.present == false})
+            .verifyComplete()
     }
 
     def "delete passes the user ID through to be deleted"() {
-        when:
-        controller.delete(100)
+        PublisherProbe probe = PublisherProbe.empty()
+        service.deleteUser(100) >> probe.mono()
 
-        then:
-        1 * service.deleteUser(100)
+        expect:
+        StepVerifier.create(controller.delete(100))
+            .verifyComplete()
+        probe.assertWasSubscribed()
+        probe.assertWasRequested()
+        probe.assertWasNotCancelled()
     }
 
     def "change password passes the user ID, current password, and new passwords through"() {
-        when:
-        controller.changePassword(100, PasswordChangeRequest.create("current", "new"))
+        PublisherProbe probe = PublisherProbe.empty()
+        service.updateUserPassword(100, "current", "new") >> probe.mono()
 
-        then:
-        1 * service.updateUserPassword(100, "current", "new")
+        expect:
+        StepVerifier.create(controller.changePassword(100, PasswordChangeRequest.create("current", "new")))
+            .verifyComplete()
+        probe.assertWasSubscribed()
+        probe.assertWasRequested()
+        probe.assertWasNotCancelled()
     }
 
     def "granting a role passes the user and role IDs through"() {
-        when:
-        controller.grantRole(100, 200)
+        PublisherProbe probe = PublisherProbe.empty()
+        service.grantRoleToUser(100, 200) >> probe.mono()
 
-        then:
-        1 * service.grantRoleToUser(100, 200)
+        expect:
+        StepVerifier.create(controller.grantRole(100, 200))
+            .verifyComplete()
+        probe.assertWasSubscribed()
+        probe.assertWasRequested()
+        probe.assertWasNotCancelled()
     }
 
     def "removing a role passes the user and role IDs through"() {
-        when:
-        controller.removeRole(200, 300)
+        PublisherProbe probe = PublisherProbe.empty()
+        service.removeRoleFromUser(200, 300) >> probe.mono()
 
-        then:
-        1 * service.removeRoleFromUser(200, 300)
+        expect:
+        StepVerifier.create(controller.removeRole(200, 300))
+            .verifyComplete()
+        probe.assertWasSubscribed()
+        probe.assertWasRequested()
+        probe.assertWasNotCancelled()
     }
 }
