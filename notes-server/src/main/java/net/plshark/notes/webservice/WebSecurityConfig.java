5410e3df8ed440400d2305ffa7d91b9edd9f8e32
alex <alex@plshark.net>
2018-12-27 12:07:24 -0500

    Set up partial auth service authentication

--- notes-server/src/main/java/net/plshark/notes/webservice/WebSecurityConfig.java
+++ notes-server/src/main/java/net/plshark/notes/webservice/WebSecurityConfig.java
@@ -1,30 +1,24 @@
 package net.plshark.notes.webservice;
 
-import javax.inject.Inject;
-
+import com.auth0.jwt.JWTVerifier;
+import net.plshark.auth.jwt.HttpBearerBuilder;
+import net.plshark.auth.jwt.JwtReactiveAuthenticationManager;
+import net.plshark.auth.throttle.IpThrottlingFilter;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Import;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-
-import net.plshark.auth.throttle.LoginAttemptService;
-import net.plshark.auth.throttle.LoginAttemptThrottlingFilter;
-import net.plshark.auth.throttle.LoginThrottlingConfig;
-import net.plshark.auth.throttle.impl.BasicAuthenticationUsernameExtractor;
+import org.springframework.security.web.server.context.NoOpServerSecurityContextRepository;
 
 /**
  * Notes web security configuration
  */
 @EnableWebFluxSecurity
-@Import(LoginThrottlingConfig.class)
 public class WebSecurityConfig {
 
-    @Inject
-    private LoginAttemptService loginAttemptService;
+    // TODO
+    private JWTVerifier jwtVerifier;
 
     /**
      * Set up the security filter chain
@@ -33,25 +27,28 @@ public class WebSecurityConfig {
      */
     @Bean
     public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) {
+        HttpBearerBuilder builder = new HttpBearerBuilder(authenticationManager());
         http
             .authorizeExchange()
                 .anyExchange()
                     .hasRole("notes-user")
             // use basic authentication
-            .and().httpBasic()
-            .and().addFilterAt(loginAttemptThrottlingFilter(), SecurityWebFiltersOrder.HTTP_BASIC);
+            .and()
+                .authenticationManager(authenticationManager())
+                .securityContextRepository(NoOpServerSecurityContextRepository.getInstance())
+                .csrf().disable()
+                .logout().disable()
+                .addFilterAt(ipThrottlingFilter(), SecurityWebFiltersOrder.HTTP_BASIC)
+                .addFilterAt(builder.buildFilter(), SecurityWebFiltersOrder.HTTP_BASIC);
         return http.build();
     }
 
-    /**
-     * @return the encoder to use to encode passwords
-     */
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+    public JwtReactiveAuthenticationManager authenticationManager() {
+        return new JwtReactiveAuthenticationManager(jwtVerifier);
     }
 
-    private LoginAttemptThrottlingFilter loginAttemptThrottlingFilter() {
-        return new LoginAttemptThrottlingFilter(loginAttemptService, new BasicAuthenticationUsernameExtractor());
+    private IpThrottlingFilter ipThrottlingFilter() {
+        return new IpThrottlingFilter();
     }
 }
