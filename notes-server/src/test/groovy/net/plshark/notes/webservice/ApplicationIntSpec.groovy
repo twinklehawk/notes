fc6b8f61c43f19dbd3ab2e55cdb442e8d2b9c35b
alex <alex@plshark.net>
2019-01-22 21:34:41 -0500

    Make application startable

--- notes-server/src/test/groovy/net/plshark/notes/webservice/ApplicationIntSpec.groovy
+++ notes-server/src/test/groovy/net/plshark/notes/webservice/ApplicationIntSpec.groovy
@@ -4,7 +4,12 @@ import javax.inject.Inject
 import org.springframework.boot.test.context.SpringBootTest
 import spock.lang.Specification
 
-@SpringBootTest(classes = Application.class)
+@SpringBootTest(classes = [ Application.class, TestConfig.class],
+        properties = [
+                'spring.flyway.placeholders.schema=notes',
+                'spring.flyway.placeholders.username=testuser',
+                'spring.flyway.placeholders.password=pass',
+                'spring.flyway.schemas=notes' ])
 class ApplicationIntSpec extends Specification {
 
     @Inject
